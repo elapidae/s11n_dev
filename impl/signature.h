d6fac6e3a29b825260529e2274efd7b4073aed34
Sasha Gromtsev <elapidae@yandex.ru>
2019-08-21 18:52:21 +0300

    Подправлена сигнатура, подправлено, чтобы работало с std::*map. Там value_type со спецификатором const -> просто снял const volatile с plain-типа. Удалил лишние приставки и подчеркивания.

--- impl/signature.h
+++ impl/signature.h
@@ -1,9 +1,10 @@
-#pragma once
+#ifndef S11N_IMPL_SIGNATURE_H
+#define S11N_IMPL_SIGNATURE_H
 
 #include "impl/crc.h"
 #include "impl/name_of_type_from_pf.h"
 #include "impl/tuple_helper.h"
-#include "impl/type_spec.h"
+#include "impl/container_helper.h"
 
 //=======================================================================================
 //      signature -- сигнатура типа, что-то вроде отпечатка пальца.
@@ -20,6 +21,8 @@
 //  Сигнатура кортежей составляется из фигурных скобок, в которых указываются типы:
 //      "{name_of_type1,name_of_type2,...,name_of_typeN}"
 //
+//
+//  Под капотом: сделано в три стадии: <имя_типа> -> <описание> -> <кортеж>
 //
 //=======================================================================================
 namespace s11n {
@@ -53,10 +56,10 @@ namespace impl
     //===================================================================================
     //      Serial<T>::name_of_type deduction
     template<typename T, typename = void>
-    struct _s_has_serial_name_of_type : std::false_type {};
+    struct _has_serial_name_of_type : std::false_type {};
     //-----------------------------------------------------------------------------------
     template<typename T>
-    struct _s_has_serial_name_of_type
+    struct _has_serial_name_of_type
             <
                 T,
                 typename std::void_t< decltype(s11n::Serial<T>::name_of_type) >
@@ -67,7 +70,7 @@ namespace impl
     template<typename T> constexpr
     bool has_serial_name_of_type()
     {
-        return _s_has_serial_name_of_type<T>::value;
+        return _has_serial_name_of_type<T>::value;
     }
     //      Serial<T>::name_of_type deduction
     //===================================================================================
@@ -83,7 +86,7 @@ namespace impl
     };
     //-----------------------------------------------------------------------------------
     template <typename T> constexpr
-    sign_spec_1 _sign_spec_1_of()
+    sign_spec_1 sign_spec_1_of()
     {
         return  std::is_arithmetic<T>::value        ? sign_spec_1::as_plain
               : impl::has_serial_name_of_type<T>()  ? sign_spec_1::as_own_name_of_type
@@ -93,11 +96,11 @@ namespace impl
     }
     //===================================================================================
     template <typename T, impl::sign_spec_1>
-    struct _s_signature_1;
+    struct _signature_1;
     //===================================================================================
     //  as plain
     template <typename T>
-    struct _s_signature_1<T, impl::sign_spec_1::as_plain>
+    struct _signature_1<T, impl::sign_spec_1::as_plain>
     {
         //-------------------------------------------------------------------------------
         static constexpr str_view name_of_type()
@@ -108,23 +111,25 @@ namespace impl
             static_assert( sizeof(double) == 8, "sizeof(double) != 8" );
             static_assert( sizeof(T)      <= 8, "size is too big..."  );
 
+            using TT = typename std::remove_cv<T>::type;
+
             return
-                std::is_same<char,T>::value     ? impl::str_view{"char", 4}    :
-                std::is_same<bool,T>::value     ? impl::str_view{"bool", 4}    :
+                std::is_same<char,TT>::value     ? impl::str_view{"char", 4}    :
+                std::is_same<bool,TT>::value     ? impl::str_view{"bool", 4}    :
 
-                std::is_same<int8_t,T>::value   ? impl::str_view{"int8", 4}    :
-                std::is_same<int16_t,T>::value  ? impl::str_view{"int16", 5}   :
-                std::is_same<int32_t,T>::value  ? impl::str_view{"int32", 5}   :
-                std::is_same<int64_t,T>::value  ? impl::str_view{"int64", 5}   :
+                std::is_same<int8_t,TT>::value   ? impl::str_view{"int8", 4}    :
+                std::is_same<int16_t,TT>::value  ? impl::str_view{"int16", 5}   :
+                std::is_same<int32_t,TT>::value  ? impl::str_view{"int32", 5}   :
+                std::is_same<int64_t,TT>::value  ? impl::str_view{"int64", 5}   :
 
-                std::is_same<uint8_t,T>::value  ? impl::str_view{"uint8", 5}   :
-                std::is_same<uint16_t,T>::value ? impl::str_view{"uint16", 6}  :
-                std::is_same<uint32_t,T>::value ? impl::str_view{"uint32", 6}  :
-                std::is_same<uint64_t,T>::value ? impl::str_view{"uint64", 6}  :
+                std::is_same<uint8_t,TT>::value  ? impl::str_view{"uint8", 5}   :
+                std::is_same<uint16_t,TT>::value ? impl::str_view{"uint16", 6}  :
+                std::is_same<uint32_t,TT>::value ? impl::str_view{"uint32", 6}  :
+                std::is_same<uint64_t,TT>::value ? impl::str_view{"uint64", 6}  :
 
-                std::is_same<float,T>::value    ? impl::str_view{"float32", 7} :
-                std::is_same<double,T>::value   ? impl::str_view{"float64", 7} :
-                                                  impl::str_view{"!ERROR!", 7};
+                std::is_same<float,TT>::value    ? impl::str_view{"float32", 7} :
+                std::is_same<double,TT>::value   ? impl::str_view{"float64", 7} :
+                                                   impl::str_view{"!ERROR!", 7};
         }
         //-------------------------------------------------------------------------------
         static std::string signature()
@@ -142,7 +147,7 @@ namespace impl
     //===================================================================================
     //  as_own_name_of_type
     template <typename T>
-    struct _s_signature_1<T, impl::sign_spec_1::as_own_name_of_type>
+    struct _signature_1<T, impl::sign_spec_1::as_own_name_of_type>
     {
         //-------------------------------------------------------------------------------
         static constexpr str_view name_of_type()
@@ -169,7 +174,7 @@ namespace impl
     //===================================================================================
     //  as_container
     template <typename T>
-    struct _s_signature_1<T, impl::sign_spec_1::as_container>
+    struct _signature_1<T, impl::sign_spec_1::as_container>
     {
         //-------------------------------------------------------------------------------
         using _vt = typename T::value_type;
@@ -200,8 +205,9 @@ namespace impl
     //===================================================================================
     //  as_tuple
     //-----------------------------------------------------------------------------------
+    //  signature_tuple
     template <int idx, typename Tup>
-    struct _s_signature_tuple
+    struct _signature_tuple
     {
         //-------------------------------------------------------------------------------
         static void signature( std::string * res )
@@ -213,13 +219,13 @@ namespace impl
             if (next_idx > 0)
                 res->push_back(',');
 
-            _s_signature_tuple<next_idx,Tup>::signature( res );
+            _signature_tuple<next_idx,Tup>::signature( res );
         }
         //-------------------------------------------------------------------------------
         static constexpr uint32_t crc( uint32_t prev )
         {
             using t = tuple_element<Tup,idx>;
-            using next = _s_signature_tuple<tuple_next_idx<idx,Tup>(),Tup>;
+            using next = _signature_tuple<tuple_next_idx<idx,Tup>(),Tup>;
 
             return                    
                 next::crc
@@ -235,7 +241,7 @@ namespace impl
     };
     //-----------------------------------------------------------------------------------
     template <typename Tup>
-    struct _s_signature_tuple<-1,Tup>
+    struct _signature_tuple<-1,Tup>
     {
         //-------------------------------------------------------------------------------
         static void signature( std::string * res )
@@ -250,10 +256,11 @@ namespace impl
         }
         //-------------------------------------------------------------------------------
     };
-    //-----------------------------------------------------------------------------------
-    //-----------------------------------------------------------------------------------
+    //  signature_tuple
+    //===================================================================================
+    //  signature_1
     template <typename T>
-    struct _s_signature_1<T, impl::sign_spec_1::as_tuple>
+    struct _signature_1<T, impl::sign_spec_1::as_tuple>
     {
         //-------------------------------------------------------------------------------
         static std::string signature()
@@ -262,7 +269,7 @@ namespace impl
             res.push_back( '{' );
 
             constexpr auto idx = tuple_start_idx<T>();
-            _s_signature_tuple<idx,T>::signature( &res );
+            _signature_tuple<idx,T>::signature( &res );
 
             return res;
         }
@@ -270,7 +277,7 @@ namespace impl
         static constexpr uint32_t crc( uint32_t prev )
         {
             return
-                _s_signature_tuple<tuple_start_idx<T>(),T>::crc
+                _signature_tuple<tuple_start_idx<T>(),T>::crc
                 (
                     calc_crc_ch( '{', prev )
                 );
@@ -281,7 +288,7 @@ namespace impl
     //===================================================================================
     //  as_pretty_func
     template <typename T>
-    struct _s_signature_1<T, impl::sign_spec_1::as_pretty_func>
+    struct _signature_1<T, impl::sign_spec_1::as_pretty_func>
     {
         //-------------------------------------------------------------------------------
         static constexpr str_view name_of_type()
@@ -303,15 +310,15 @@ namespace impl
     //  as_pretty_func
     //===================================================================================
     template <typename T>
-    std::string _signature_1()
+    std::string signature_1()
     {
-        return _s_signature_1< T, _sign_spec_1_of<T>() >::signature();
+        return _signature_1<T,sign_spec_1_of<T>()>::signature();
     }
     //-----------------------------------------------------------------------------------
     template <typename T>
-    constexpr uint32_t _calc_crc_1( uint32_t prev )
+    constexpr uint32_t calc_crc_1( uint32_t prev )
     {
-        return _s_signature_1< T, _sign_spec_1_of<T>() >::crc( prev );
+        return _signature_1<T,sign_spec_1_of<T>()>::crc( prev );
     }
     //===================================================================================
     //      signature_1
@@ -323,10 +330,10 @@ namespace impl
     //===================================================================================
     //      Serial<T>::description deduction
     template<typename T, typename = void>
-    struct _s_has_serial_description : std::false_type {};
+    struct _has_serial_description : std::false_type {};
     //-----------------------------------------------------------------------------------
     template<typename T>
-    struct _s_has_serial_description
+    struct _has_serial_description
             <
                 T,
                 typename std::void_t< decltype(s11n::Serial<T>::description) >
@@ -335,9 +342,9 @@ namespace impl
     {};
     //-----------------------------------------------------------------------------------
     template<typename T> constexpr
-    bool __has_serial_description()
+    bool has_serial_description()
     {
-        return _s_has_serial_description<T>::value;
+        return _has_serial_description<T>::value;
     }
     //      Serial<T>::description deduction
     //===================================================================================
@@ -348,17 +355,17 @@ namespace impl
     };
     //-----------------------------------------------------------------------------------
     template<typename T>
-    constexpr sign_spec_2 _sign_spec_2_of()
+    constexpr sign_spec_2 sign_spec_2_of()
     {
-        return __has_serial_description<T>() ? sign_spec_2::with_description
-                                             : sign_spec_2::without_description;
+        return has_serial_description<T>() ? sign_spec_2::with_description
+                                           : sign_spec_2::without_description;
     }
     //===================================================================================
     template<typename T, sign_spec_2>
-    struct _s_signature_2;
+    struct _signature_2;
     //===================================================================================
     template<typename T>
-    struct _s_signature_2<T,sign_spec_2::without_description>
+    struct _signature_2<T,sign_spec_2::without_description>
     {
         //-------------------------------------------------------------------------------
         static constexpr uint32_t crc( uint32_t prev )
@@ -372,7 +379,7 @@ namespace impl
     };
     //===================================================================================
     template<typename T>
-    struct _s_signature_2<T,sign_spec_2::with_description>
+    struct _signature_2<T,sign_spec_2::with_description>
     {
         //-------------------------------------------------------------------------------
         static constexpr impl::str_view description()
@@ -395,7 +402,7 @@ namespace impl
                     );
         }
         //-------------------------------------------------------------------------------
-        static void signature( std::string *res )
+        static void signature( std::string * res )
         {
             res->push_back( '(' );
             res->append( description() );
@@ -405,19 +412,19 @@ namespace impl
     };
     //-----------------------------------------------------------------------------------
     template <typename T>
-    std::string _signature_2()
+    std::string signature_2()
     {
-        auto res = _signature_1<T>();
-        _s_signature_2<T, _sign_spec_2_of<T>()>::signature( &res );
+        auto res = signature_1<T>();
+        _signature_2<T,sign_spec_2_of<T>()>::signature( &res );
         return res;
     }
     //-----------------------------------------------------------------------------------
     template <typename T>
-    constexpr uint32_t _calc_crc_2( uint32_t prev )
+    constexpr uint32_t calc_crc_2( uint32_t prev )
     {
-        return _s_signature_2< T, _sign_spec_2_of<T>() >::crc
+        return _signature_2<T,sign_spec_2_of<T>()>::crc
                 (
-                    _calc_crc_1<T>( prev )
+                    calc_crc_1<T>( prev )
                 );
     }
     //===================================================================================
@@ -428,24 +435,24 @@ namespace impl
     //===================================================================================
     //      signature_3 = signature_2 + serial tuple specialization
     //===================================================================================
-    enum class _sign_spec_3
+    enum class sign_spec_3
     {
         with_serial_tuple,
         without_serial_tuple
     };
     //-----------------------------------------------------------------------------------
     template <typename T>
-    constexpr _sign_spec_3 _sign_spec_3_of()
+    constexpr sign_spec_3 sign_spec_3_of()
     {
-        return has_serial_tuple<T>() ? _sign_spec_3::with_serial_tuple
-                                     : _sign_spec_3::without_serial_tuple;
+        return has_serial_tuple<T>() ? sign_spec_3::with_serial_tuple
+                                     : sign_spec_3::without_serial_tuple;
     }
     //===================================================================================
-    template<typename T,_sign_spec_3>
-    struct _s_signature_3;
+    template<typename T,sign_spec_3>
+    struct _signature_3;
     //-----------------------------------------------------------------------------------
     template<typename T>
-    struct _s_signature_3<T,_sign_spec_3::without_serial_tuple>
+    struct _signature_3<T,sign_spec_3::without_serial_tuple>
     {
         //-------------------------------------------------------------------------------
         static constexpr uint32_t crc( uint32_t prev )
@@ -459,7 +466,7 @@ namespace impl
     };
     //-----------------------------------------------------------------------------------
     template<typename T>
-    struct _s_signature_3<T,_sign_spec_3::with_serial_tuple>
+    struct _signature_3<T,sign_spec_3::with_serial_tuple>
     {
         //-------------------------------------------------------------------------------
         using Tup = decltype( s11n::Serial<T>::to_tuple(std::declval<T>()) );
@@ -477,19 +484,19 @@ namespace impl
     };
     //-----------------------------------------------------------------------------------
     template <typename T>
-    std::string _signature_3()
+    std::string signature_3()
     {
-        auto res = _signature_2<T>();
-        _s_signature_3<T, _sign_spec_3_of<T>()>::signature( &res );
+        auto res = signature_2<T>();
+        _signature_3<T,sign_spec_3_of<T>()>::signature( &res );
         return res;
     }
     //-----------------------------------------------------------------------------------
     template <typename T>
-    constexpr uint32_t _calc_crc_3( uint32_t prev )
+    constexpr uint32_t calc_crc_3( uint32_t prev )
     {
-        return _s_signature_3< T, _sign_spec_3_of<T>() >::crc
+        return _signature_3<T,sign_spec_3_of<T>()>::crc
                 (
-                    _calc_crc_2<T>( prev )
+                    calc_crc_2<T>( prev )
                 );
     }
     //===================================================================================
@@ -502,25 +509,25 @@ namespace impl
     template <typename T>
     std::string signature()
     {
-        return _signature_3<T>();
+        return signature_3<T>();
     }
     //-----------------------------------------------------------------------------------
     template <typename T>
     constexpr uint32_t calc_crc_T( uint32_t prev )
     {
-        return _calc_crc_3<T>( prev );
+        return calc_crc_3<T>( prev );
     }
     //-----------------------------------------------------------------------------------
     template <typename T>
     constexpr uint32_t crc()
     {
-        return _crc_last_xor ^ calc_crc_T<T>( _crc_first_val );
+        return crc_last_xor ^ calc_crc_T<T>( crc_first_val );
     }
     //===================================================================================
     //      Result wrap
     //===================================================================================
-
-
 } // namespace impl
 } // namespace s11n
 //=======================================================================================
+
+#endif // S11N_IMPL_SIGNATURE_H
